8890de77d857f0d46ab16518c8092dfb57b2d32b
Andrew <andrew@deadcow.ru>
2017-12-19 15:30:43 +0700

    ConditionallyEnabledGUIBlock

---
+++ Editor/ConditionallyEnabledGUIBlock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class ConditionallyEnabledGUIBlock : IDisposable
+{
+	private readonly bool _originalState;
+
+	public ConditionallyEnabledGUIBlock(bool condition)
+	{
+		_originalState = GUI.enabled;
+		GUI.enabled = condition;
+	}
+
+	public void Dispose()
+	{
+		GUI.enabled = _originalState;
+	}
+}
