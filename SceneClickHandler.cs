3bc5a5a53d531655f0b92c658d5cf694dd6e1276
Deadcow <andrew@deadcow.ru>
2018-02-06 14:38:04 +0300

    SceneClickHandler is used to cast ray to sceneView and get callback when clicked on something. Like, to click on the floor in sceneView and place something on this spot via custom editor tool

---
+++ SceneClickHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+public class SceneClickHandler
+{
+	public SceneClickHandler(Action<Vector3> onClick, bool singleClick)
+	{
+		_singleClickHandler = singleClick;
+		_onClick = onClick;
+
+		SceneView.onSceneGUIDelegate += OnSceneGUI;
+	}
+	
+	~SceneClickHandler()
+	{
+		SceneView.onSceneGUIDelegate -= OnSceneGUI;
+	}
+
+	public void ToggleState() => Enabled = !Enabled;
+	public bool Enabled { get; set; }
+
+	private readonly bool _singleClickHandler;
+	private readonly Action<Vector3> _onClick;
+
+	private void OnSceneGUI(SceneView sceneview)
+	{
+		if (!Enabled) return;
+
+		RaycastHit hit;
+		var ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+		if (Physics.Raycast(ray, out hit))
+		{
+			Handles.DrawWireCube(hit.point, Vector3.one * .2f);
+		}
+
+		if (Handles.Button(Vector3.zero, SceneView.currentDrawingSceneView.rotation, 30, 5000, Handles.RectangleHandleCap))
+		{
+			if (_singleClickHandler) Enabled = false;
+
+			_onClick(hit.point);
+			
+			Event.current.Use();
+			HandleUtility.Repaint();
+		}
+	}
+
+}
