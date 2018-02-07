26498d62970aaa47ec40c57175d9393814fc15d8
Deadcow <andrew@deadcow.ru>
2018-02-07 17:09:08 +0300

    Some sugar to easilly create sprites out of textures

---
+++ Extensions/MyTextureExtensions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class MyTextureExtensions
+{
+	public static Sprite AsSprite(this Texture2D texture)
+	{
+		var rect = new Rect(0, 0, texture.width, texture.height);
+		var pivot = new Vector2(0.5f, 0.5f);
+		return Sprite.Create(texture, rect, pivot);
+	}
+}
