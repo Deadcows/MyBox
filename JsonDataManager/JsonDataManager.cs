66440564fb6225dd888d3c8b6d56642988fc519c
Andrey Rumak <deadcows@yandex.ru>
2018-03-14 11:02:33 +0300

    Scripts used by UnityEditor moved to Editor folder, to not prevent project build + some code moved to #if UNITY_EDITOR blocks.

--- JsonDataManager/JsonDataManager.cs
+++ JsonDataManager/JsonDataManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 
 
@@ -12,12 +14,13 @@ public static class JsonDataManager
 	//TODO: Заменить менеджер на базовый тип (вместо SO)?
 	//TODO: Добавить отдельный файл для самого DataManager, в котором отслеживать фолдауты или типа того?
 
-	public const string SettingsFolder = "Misc/Configs";
+	public const string EditorSettingsFolder = "Misc/Configs";
+	public const string BuildSettingsFolder = "Configs";
 	private static readonly Dictionary<string, ScriptableObject> Cache = new Dictionary<string, ScriptableObject>();
 
 	private static string GetPath(string type)
 	{
-		return Path.Combine(Application.dataPath, SettingsFolder, type + ".json");
+		return Path.Combine(Application.dataPath, Application.isEditor ? EditorSettingsFolder : BuildSettingsFolder, type + ".json");
 	}
 
 	public static T FromJson<T>(bool directlyFromFile = false) where T : ScriptableObject, new()
@@ -39,15 +42,16 @@ public static class JsonDataManager
 		return data;
 	}
 
-
-	public static void SaveJson<T>(T so) where T : ScriptableObject
-	{
-		string typeString = typeof(T).ToString();
+    public static void SaveJson<T>(T so) where T : ScriptableObject
+    {
+#if UNITY_EDITOR
+        string typeString = typeof(T).ToString();
 		var jsonString = JsonUtility.ToJson(so, true);
 		var path = GetPath(typeString);
 
 		File.WriteAllText(path, jsonString);
 
 		AssetDatabase.Refresh();
-	}
+#endif
+    }
 }
