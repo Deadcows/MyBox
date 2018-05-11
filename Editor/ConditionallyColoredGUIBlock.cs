81c0c7abbef09227f7dc332e2f6bdf9262fd9cc4
Andrey Rumak <deadcows@yandex.ru>
2018-05-11 16:06:54 +0300

    Conditionally Colored GUI Block... yeah, it's time to clean this sruff somehow

---
+++ Editor/ConditionallyColoredGUIBlock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class ConditionallyColoredGUIBlock : IDisposable
+{
+	private readonly Color _originalColor;
+
+	public ConditionallyColoredGUIBlock(bool condition, Color color)
+	{
+		_originalColor = GUI.color;
+
+		if (condition) GUI.color = color;
+	}
+
+	public void Dispose()
+	{
+		GUI.color = _originalColor;
+	}
+}
