c3cd83d86169c70d440617ba8acc12b5c44803b7
Andrey Rumak <deadcows@yandex.ru>
2018-05-11 16:03:19 +0300

    RangedFloat type along with [MinMaxRangeAttribute]

---
+++ MinMaxRangeAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class MinMaxRangeAttribute : Attribute
+{
+	public MinMaxRangeAttribute(float min, float max)
+	{
+		Min = min;
+		Max = max;
+	}
+	public float Min { get; }
+	public float Max { get; }
+}
---
+++ RangedFloat.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+[Serializable]
+public struct RangedFloat
+{
+	public float Min;
+	public float Max;
+}
