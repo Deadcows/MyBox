3468eb251b50cee6077933f35799bd96e32dd7cf
Andrey Rumak <deadcows@yandex.ru>
2018-07-09 10:55:43 +0300

    UniqueComponent for ECS

--- ECS/UniqueComponent.cs
+++ ECS/UniqueComponent.cs
@@ -11,8 +11,35 @@ public struct UniqueComponent<T> where T : Component
 	{
 		get
 		{
-			Debug.Assert(Components.Length == 1, "UniqueComponent: There is more tnan one instance of component");
+			Debug.Assert(Components.Length == 1, "UniqueComponent: There is more/less tnan one instance of component");
 			return Components[0];
+		}
+	}
+
+	public Entity EntityInstance => Entities[0];
+}
+
+public struct UniqueTransformComponent<T> where T : Component
+{
+	public ComponentArray<T> Components;
+	public ComponentArray<Transform> Transform;
+	public EntityArray Entities;
+
+	public T Instance
+	{
+		get
+		{
+			Debug.Assert(Components.Length == 1, "UniqueComponent: There is more/less tnan one instance of component");
+			return Components[0];
+		}
+	}
+
+	public Transform TransformInstance
+	{
+		get
+		{
+			Debug.Assert(Components.Length == 1, "UniqueComponent: There is more/less tnan one instance of component");
+			return Transform[0];
 		}
 	}
 
@@ -29,7 +56,7 @@ public struct UniqueComponentData<T> where T : struct, IComponentData
 	{
 		get
 		{
-			Debug.Assert(Components.Length == 1, "UniqueComponent: There is more tnan one instance of component");
+			Debug.Assert(Components.Length == 1, "UniqueComponent: There is more/less tnan one instance of component");
 			return Components[0];
 		}
 	}
