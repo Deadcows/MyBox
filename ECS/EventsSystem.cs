e3ba4cc74e47e8f07b5a9908087f224191d0082a
Andrew <andrew@deadcow.ru>
2018-08-31 17:34:30 +0300

    EventsSystem updated

--- ECS/EventsSystem.cs
+++ ECS/EventsSystem.cs
@@ -1,102 +1,99 @@
-using System.Collections.Generic;
 using Unity.Entities;
-using UnityEngine;
 using UnityEngine.Experimental.PlayerLoop;
 
 
 public struct Event<T> where T : struct, IComponentData
 {
-	[Unity.Collections.ReadOnly] public ComponentDataArray<T> _event;
-		
-	public bool Fired => _event.Length > 0;
-	public T Data => _event[0];
+	[Unity.Collections.ReadOnly] public ComponentDataArray<T> Data;
+	[Unity.Collections.ReadOnly] public ComponentDataArray<EventsSystem.EventActiveComponent> EventActive;
+
+	public bool Fired => Data.Length > 0;
 }
-	
+
 public struct SharedDataEvent<T> where T : struct, ISharedComponentData
 {
-	[Unity.Collections.ReadOnly] public SharedComponentDataArray<T> _event;
+	[Unity.Collections.ReadOnly] public SharedComponentDataArray<T> Data;
+	[Unity.Collections.ReadOnly] public ComponentDataArray<EventsSystem.EventActiveComponent> EventActive;
 
-	public bool Fired => _event.Length > 0;
-	public T Data => _event[0];
+	public bool Fired => Data.Length > 0;
 }
 
-[UpdateInGroup(typeof(PreUpdate))]
+
+[UpdateInGroup(typeof(PreUpdate)), AlwaysUpdateSystem]
 public class EventsSystem : ComponentSystem
 {
-
-	private readonly Dictionary<ComponentType, (ComponentGroup Group, EntityArchetype Archetype)> _registeredEvents =
-		new Dictionary<ComponentType, (ComponentGroup Group, EntityArchetype Archetype)>();
-
-	private readonly Dictionary<ComponentSystem, ComponentType> _firedEvents = new Dictionary<ComponentSystem, ComponentType>();
-
-	
-	public void RegisterEvent(ComponentType componentType)
-	{
-		if (_registeredEvents.ContainsKey(componentType)) return;
-
-		var eventGroup = GetComponentGroup(componentType);
-		var eventArchetype = EntityManager.CreateArchetype(componentType);
-		_registeredEvents.Add(componentType, (eventGroup, eventArchetype));
-	}
-
-
-	public void ShootEvent<T>(ComponentSystem system) where T : struct, IComponentData
-	{
-		ShootEvent(system, new T());
-	}
-
-	public void ShootEvent<T>(ComponentSystem system, T eventComponent) where T : struct, IComponentData
-	{
-		ComponentType eventType = ComponentType.Create<T>();
-		
-		if (!_registeredEvents.ContainsKey(eventType)) RegisterEvent(eventType);
-		
-		system.PostUpdateCommands.CreateEntity(_registeredEvents[eventType].Archetype);
-		system.PostUpdateCommands.SetComponent(eventComponent);
-
-		_firedEvents.Add(system, eventType);
-		Debug.Log("Event shooted!");
-	}
-	
-	
-	public void ShootSharedDataEvent<T>(ComponentSystem system, T eventComponent) where T : struct, ISharedComponentData
-	{
-		ComponentType eventType = ComponentType.Create<T>();
-		
-		if (!_registeredEvents.ContainsKey(eventType)) RegisterEvent(eventType);
-		
-		system.PostUpdateCommands.CreateEntity(_registeredEvents[eventType].Archetype);
-		system.PostUpdateCommands.SetSharedComponent(eventComponent);
-
-		_firedEvents.Add(system, eventType);
-		Debug.Log("Shared event shooted!");
-	}
-
-
-	public void ResetEvents(ComponentSystem system) 
-	{
-		if (!_firedEvents.ContainsKey(system)) return;
-		var eventType = _firedEvents[system];
-		var group = _registeredEvents[eventType].Group;
-
-		var eventEntities = group.GetEntityArray();
-		for (var i = 0; i < eventEntities.Length; i++)
-		{
-			system.PostUpdateCommands.DestroyEntity(eventEntities[i]);
-			Debug.Log("Reseted");
-		}
-		
-		_firedEvents.Remove(system);
-	}
-	
-	
+	private ComponentGroup _firedEvents;
+	private ComponentGroup _firedActiveEvents;
 
 	protected override void OnCreateManager(int capacity)
 	{
-		Enabled = false;
+		_firedEvents = GetComponentGroup(ComponentType.Create<EventComponent>(), ComponentType.Subtractive<EventActiveComponent>());
+		_firedActiveEvents = GetComponentGroup(ComponentType.Create<EventComponent>(), ComponentType.Create<EventActiveComponent>());
 	}
 
 	protected override void OnUpdate()
 	{
+		DestroyActiveEvents();
+		ActivateInactiveEvents();
+
+		void DestroyActiveEvents()
+		{
+			var activeEvents = _firedActiveEvents.GetEntityArray();
+
+			for (var i = 0; i < activeEvents.Length; i++)
+			{
+				var entity = activeEvents[i];
+				PostUpdateCommands.DestroyEntity(entity);
+			}
+		}
+
+		void ActivateInactiveEvents()
+		{
+			var inactiveEvents = _firedEvents.GetEntityArray();
+			for (var i = 0; i < inactiveEvents.Length; i++)
+			{
+				var entity = inactiveEvents[i];
+				PostUpdateCommands.AddComponent(entity, new EventActiveComponent());
+			}
+		}
+	}
+
+	public struct EventComponent : IComponentData
+	{
+	}
+
+	public struct EventActiveComponent : IComponentData
+	{
+	}
+}
+
+public static class EventsSystemExtension
+{
+	public static void AddEvent<T>(this EntityCommandBuffer buffer, T eventComponent) where T : struct, IComponentData
+	{
+		buffer.CreateEntity();
+		buffer.AddComponent(eventComponent);
+		buffer.AddComponent(new EventsSystem.EventComponent());
+	}
+
+	public static void AddEvent<T>(this EntityManager manager, T eventComponent) where T : struct, IComponentData
+	{
+		var eventEntity = manager.CreateEntity();
+		manager.AddComponentData(eventEntity, eventComponent);
+		manager.AddComponent(eventEntity, ComponentType.Create<EventsSystem.EventComponent>());
+	}
+
+	public static void AddSharedEvent<T>(this EntityCommandBuffer buffer, T eventComponent) where T : struct, ISharedComponentData
+	{
+		buffer.CreateEntity();
+		buffer.AddSharedComponent(eventComponent);
+		buffer.AddComponent(new EventsSystem.EventComponent());
+	}
+
+	public static void AddSharedEvent<T>(this EntityManager manager, T eventComponent) where T : struct, ISharedComponentData
+	{
+		var eventEntity = manager.CreateEntity();
+		manager.AddSharedComponentData(eventEntity, eventComponent);
+		manager.AddComponent(eventEntity, ComponentType.Create<EventsSystem.EventComponent>());
 	}
 }
