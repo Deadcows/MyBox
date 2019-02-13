83399367f4cd189d8629e5a971ab8869e2bd470e
Andrew <andrew@deadcow.ru>
2019-02-13 16:35:51 +0300

    I'll cleanup Myandles later. It's a mess tightly coupled with my current project :(

--- Editor/MyHandles.cs
+++ Editor/MyHandles.cs
@@ -24,6 +24,24 @@ public static class MyHandles
 		}
 	}
 
+	public static void DrawDirectionalLine(Vector3 fromPos, Vector3 toPos, float screenSpaceSize = 3, float arrowDensity = .5f)
+	{
+		var arrowSize = screenSpaceSize / 4;
+		
+		Handles.DrawLine(fromPos, toPos);
+
+		var direction = toPos - fromPos;
+		var distance = Vector3.Distance(fromPos, toPos);
+		var arrowsCount = (int)(distance / arrowDensity);
+		var delta = 1f / arrowsCount;
+		for (int i = 1; i <= arrowsCount; i++)
+		{
+			var currentDelta = delta * i;
+			var currentPosition = Vector3.Lerp(fromPos, toPos, currentDelta);
+			DrawTinyArrow(currentPosition, direction, arrowSize);
+		}
+	}
+	
 	public static void DrawTinyArrow(Vector3 position, Vector3 direction, float headLength = 0.25f, float headAngle = 20.0f)
 	{
 		Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 + headAngle, 0) * new Vector3(0, 0, 1);
@@ -60,6 +78,8 @@ public static class MyHandles
 		{
 			var cornerA = corners[i - 1];
 			var cornerB = corners[i];
+			var size = HandleUtility.GetHandleSize(new Vector3(cornerA.x, cornerA.y, cornerA.z));
+			DrawDirectionalLine(cornerA, cornerB, size, size);
 			Handles.DrawLine(cornerA, cornerB);
 		}
 	}
