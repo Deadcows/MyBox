c082066c2cb8ba05b7e2e5cde56e227ec8a25ed1
Andrew <andrew@deadcow.ru>
2019-03-03 19:18:58 +0300

    Optional[Float/Int/String/Component/etc] type with a way to create new optional types (with checkbox). OptionalMinMax type

---
+++ Types/Optional.cs
@@ -0,0 +1,62 @@
+using System;
+using MyBox.Internal;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+#endif
+
+namespace MyBox
+{
+	[Serializable] public class OptionalFloat : Optional<float> { }
+	[Serializable] public class OptionalInt : Optional<int> { }
+	[Serializable] public class OptionalString : Optional<string> { }
+	[Serializable] public class OptionalGameObject : Optional<GameObject> { }
+	[Serializable] public class OptionalComponent : Optional<Component> { }
+}
+
+namespace MyBox.Internal
+{
+	[Serializable]
+	public class Optional<T> : OptionalParent
+	{
+		public bool IsSet;
+		public T Value;
+	}
+	
+	[Serializable]
+	public class OptionalParent {}
+}
+
+#if UNITY_EDITOR
+namespace MyBox.Internal
+{
+	[CustomPropertyDrawer(typeof(OptionalParent), true)]
+	public class OptionalFloatPropertyDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			EditorGUI.BeginProperty(position, label, property);
+			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+			var value = property.FindPropertyRelative("Value");
+			var isSet = property.FindPropertyRelative("IsSet");
+
+			var checkWidth = 14;
+			var spaceWidth = 4;
+			var valWidth = position.width - checkWidth - spaceWidth;
+			
+			position.width = checkWidth;
+			isSet.boolValue = EditorGUI.Toggle(position, GUIContent.none, isSet.boolValue);
+			
+			position.x += checkWidth + spaceWidth;
+			position.width = valWidth;
+			if (isSet.boolValue) EditorGUI.PropertyField(position, value, GUIContent.none);
+
+			if (GUI.changed) property.serializedObject.ApplyModifiedProperties();
+
+			EditorGUI.EndProperty();
+		}
+	}
+}
+#endif
---
+++ Types/OptionalMinMax.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+#endif
+
+namespace MyBox
+{
+	/// <summary>
+	/// Type to set Min/Max values but with optional Min and Max
+	/// </summary>
+	[Serializable]
+	public struct OptionalMinMax
+	{
+		public bool MinIsSet;
+		public bool MaxIsSet;
+		
+		public float Min;
+		public float Max;
+
+		public float GetFixed(float value)
+		{
+			if (MinIsSet && value < Min) value = Min;
+			if (MaxIsSet && value > Max) value = Max;
+
+			return value;
+		}
+
+		public OptionalMinMax(bool minIsSet, bool maxIsSet, float min, float max)
+		{
+			MinIsSet = minIsSet;
+			MaxIsSet = maxIsSet;
+			Min = min;
+			Max = max;
+		}
+	}
+}
+
+#if UNITY_EDITOR
+namespace MyBox.Internal
+{
+	[CustomPropertyDrawer(typeof (OptionalMinMax))]
+	public class MinMaxFloatPropertyDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			EditorGUI.BeginProperty(position, label, property);
+			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+			var minProp = property.FindPropertyRelative("Min");
+			var maxProp = property.FindPropertyRelative("Max");
+			var minCheckProp = property.FindPropertyRelative("MinIsSet");
+			var maxCheckProp = property.FindPropertyRelative("MaxIsSet");
+
+			var minMaxLabel = "Min : Max";
+			var labelWidth = 58;
+			var checkWidth = 14;
+			var spaceWidth = 4;
+			var valWidth = (position.width / 2) - (labelWidth / 2f) - (spaceWidth * 4) - checkWidth + 2;
+			
+			position.width = valWidth;
+			if (minCheckProp.boolValue) EditorGUI.PropertyField(position, minProp, GUIContent.none);
+			
+			position.x += valWidth + spaceWidth;
+			position.width = checkWidth;
+			minCheckProp.boolValue = EditorGUI.Toggle(position, GUIContent.none, minCheckProp.boolValue);
+			
+			position.x += checkWidth + spaceWidth;
+			position.width = labelWidth;
+			EditorGUI.LabelField(position, minMaxLabel);
+			
+			position.x += labelWidth + spaceWidth;
+			position.width = checkWidth;
+			maxCheckProp.boolValue = EditorGUI.Toggle(position, GUIContent.none, maxCheckProp.boolValue);
+			
+			position.x += checkWidth + spaceWidth;
+			position.width = valWidth;
+			if (maxCheckProp.boolValue) EditorGUI.PropertyField(position, maxProp, GUIContent.none);
+
+			if (GUI.changed)
+			{
+				if (maxCheckProp.boolValue && maxCheckProp.boolValue)
+				{
+					if (maxProp.floatValue < minProp.floatValue) maxProp.floatValue = minProp.floatValue;
+				}
+				property.serializedObject.ApplyModifiedProperties();
+			}
+
+			EditorGUI.EndProperty();
+		}
+	}
+}
+#endif
