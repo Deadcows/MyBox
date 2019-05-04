7090dae216f1d980768e96b996db17065593ddfd
Andrew <andrew@deadcow.ru>
2019-05-04 12:44:20 +0300

    RangedInt type, with a possibility to use MinMaxRangeAttribute

--- Attributes/MinMaxRangeAttribute.cs
+++ Attributes/MinMaxRangeAttribute.cs
@@ -5,9 +5,9 @@
 
 using System;
 using UnityEngine;
-
 #if UNITY_EDITOR
 using UnityEditor;
+
 #endif
 
 namespace MyBox
@@ -30,13 +30,68 @@ namespace MyBox
 		public float Min;
 		public float Max;
 	}
+
+	[Serializable]
+	public struct RangedInt
+	{
+		public int Min;
+		public int Max;
+	}
 }
 
 #if UNITY_EDITOR
 namespace MyBox.Internal
 {
+	[CustomPropertyDrawer(typeof(RangedInt), true)]
+	public class MinMaxRangeIntAttributeDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			label = EditorGUI.BeginProperty(position, label, property);
+			position = EditorGUI.PrefixLabel(position, label);
+
+			SerializedProperty minProp = property.FindPropertyRelative("Min");
+			SerializedProperty maxProp = property.FindPropertyRelative("Max");
+
+			float minValue = minProp.intValue;
+			float maxValue = maxProp.intValue;
+
+			float rangeMin = 0;
+			float rangeMax = 1;
+
+			var ranges = (MinMaxRangeAttribute[]) fieldInfo.GetCustomAttributes(typeof(MinMaxRangeAttribute), true);
+			if (ranges.Length > 0)
+			{
+				rangeMin = ranges[0].Min;
+				rangeMax = ranges[0].Max;
+			}
+
+			const float rangeBoundsLabelWidth = 40f;
+
+			var rangeBoundsLabel1Rect = new Rect(position);
+			rangeBoundsLabel1Rect.width = rangeBoundsLabelWidth;
+			GUI.Label(rangeBoundsLabel1Rect, new GUIContent(minValue.ToString("F2")));
+			position.xMin += rangeBoundsLabelWidth;
+
+			var rangeBoundsLabel2Rect = new Rect(position);
+			rangeBoundsLabel2Rect.xMin = rangeBoundsLabel2Rect.xMax - rangeBoundsLabelWidth;
+			GUI.Label(rangeBoundsLabel2Rect, new GUIContent(maxValue.ToString("F2")));
+			position.xMax -= rangeBoundsLabelWidth;
+
+			EditorGUI.BeginChangeCheck();
+			EditorGUI.MinMaxSlider(position, ref minValue, ref maxValue, rangeMin, rangeMax);
+			if (EditorGUI.EndChangeCheck())
+			{
+				minProp.intValue = Mathf.RoundToInt(minValue);
+				maxProp.intValue = Mathf.RoundToInt(maxValue);
+			}
+
+			EditorGUI.EndProperty();
+		}
+	}
+
 	[CustomPropertyDrawer(typeof(RangedFloat), true)]
-	public class MinMaxRangeAttributeDrawer : PropertyDrawer
+	public class MinMaxRangeFloatAttributeDrawer : PropertyDrawer
 	{
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
