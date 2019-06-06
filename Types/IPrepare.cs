724d9030267e65d3d661edf4cf625434b01eeca6
Andrew <andrew@deadcow.ru>
2019-06-06 18:21:30 +0300

    IPrepare is now part of MyBox

---
+++ Types/IPrepare.cs
@@ -0,0 +1,47 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEditor;
+#endif
+
+namespace MyBox
+{
+	public interface IPrepare
+	{
+		void Prepare();
+	}
+}
+
+#if UNITY_EDITOR
+
+namespace MyBox.EditorTools
+{
+	[InitializeOnLoad]
+	public class PrepareHandler
+	{
+		public static Action OnPrepare;
+
+		static PrepareHandler()
+		{
+			MyEditorEvents.BeforePlaymode += Prepare;
+			MyEditorEvents.BeforeBuild += Prepare;
+			MyEditorEvents.OnSave += Prepare;
+		}
+
+		public static void Prepare()
+		{
+			string prepareUndoKey = "PrepareProcessUndo";
+
+			var toPrepare = MyExtensions.FindObjectsOfInterfaceAsComponents<IPrepare>();
+			foreach (var prepare in toPrepare)
+			{
+				Undo.RecordObject(prepare.Component, prepareUndoKey);
+				prepare.Interface.Prepare();
+				if (prepare.Component != null) EditorUtility.SetDirty(prepare.Component);
+			}
+
+			if (OnPrepare != null) OnPrepare();
+		}
+	}
+}
+
+#endif
