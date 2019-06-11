9190f34e25a10c191c7ac5a84202af337b161dab
Andrew <andrew@deadcow.ru>
2019-06-11 16:00:05 +0300

    MyCollections bugfixes, cleanup, new performant overloads

--- Extensions/MyCollections.cs
+++ Extensions/MyCollections.cs
@@ -66,13 +66,32 @@ namespace MyBox
 			return newArray;
 		}
 
+		
+		/// <summary>
+		/// Returns random element from collection
+		/// </summary>
+		public static T GetRandom<T>(this T[] collection)
+		{
+			return collection[Random.Range(0, collection.Length)];
+		}
+		
+		/// <summary>
+		/// Returns random element from collection
+		/// </summary>
+		public static T GetRandom<T>(this IList<T> collection)
+		{
+			return collection[Random.Range(0, collection.Count)];
+		}
+		
 		/// <summary>
 		/// Returns random element from collection
 		/// </summary>
 		public static T GetRandom<T>(this IEnumerable<T> collection)
 		{
-			return collection.ElementAt(Random.Range(0, collection.Count() - 1));
+			return collection.ElementAt(Random.Range(0, collection.Count()));
 		}
+		
+		
 
 		public static T[] GetRandomCollection<T>(this IList<T> collection, int amount)
 		{
@@ -95,16 +114,7 @@ namespace MyBox
 			return randoms;
 		}
 		
-
-		/// <summary>
-		/// Is collection null or empty
-		/// </summary>
-		public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
-		{
-			if (collection == null) return true;
-
-			return !collection.Any();
-		}
+		
 
 		/// <summary>
 		/// Is array null or empty
@@ -115,35 +125,69 @@ namespace MyBox
 
 			return collection.Length == 0;
 		}
-
+		
 		/// <summary>
-		/// Get fixed index for looping sequences. i.e. -1 will result with last element index
+		/// Is list null or empty
+		/// </summary>
+		public static bool IsNullOrEmpty<T>(this IList<T> collection)
+		{
+			if (collection == null) return true;
+
+			return collection.Count == 0;
+		}
+		
+		/// <summary>
+		/// Is collection null or empty. IEnumerable is relatively slow. Use Array or List implementation if possible
+		/// </summary>
+		public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
+		{
+			if (collection == null) return true;
+
+			return !collection.Any();
+		}
+
+		
+		
+		/// <summary>
+		/// Get next index for circular array. i.e. -1 will result with last element index, Length + 1 is 0
 		///
 		/// Example (infinite loop first->last->first):
 		/// i = myArray.NextIndex(i++);
 		/// var nextItem = myArray[i];
 		/// </summary>
-		public static int NextIndex<T>(this T[] array, int desiredPosition)
+		public static int NextIndexInCircle<T>(this T[] array, int desiredPosition)
 		{
+			if (array.IsNullOrEmpty())
+			{
+				Debug.LogError("NextIndexInCircle Caused: source array is null or empty");
+				return -1;
+			}
+			
 			if (array.Length == 0) return 0;
 			if (desiredPosition < 0) return array.Length - 1;
 			if (desiredPosition > array.Length - 1) return 0;
 			return desiredPosition;
 		}
-
+		
+		
 		/// <returns>
 		/// Returns -1 if none found
 		/// </returns>
-		public static int IndexOfItem<T>(this IEnumerable<T> items, T item)
+		public static int IndexOfItem<T>(this IEnumerable<T> collection, T item)
 		{
+			if (collection.IsNullOrEmpty())
+			{
+				Debug.LogError("NextIndexInCircle Caused: source collection is null or empty");
+				return -1;
+			}
+			
 			var index = 0;
-
-			foreach (var i in items)
+			foreach (var i in collection)
 			{
 				if (Equals(i, item)) return index;
 				++index;
 			}
-
+			
 			return -1;
 		}
 		
@@ -170,7 +214,7 @@ namespace MyBox
 		/// <summary>
 		/// Is Keys in MyDictionary is the same as some collection
 		/// </summary>
-		public static bool KeysContentsMatch<T1, T2>(this MyDictionary<T1, T2> source, IEnumerable<T1> check)
+		public static bool ContentsMatchKeys<T1, T2>(this MyDictionary<T1, T2> source, IEnumerable<T1> check)
 		{
 			if (source.IsNullOrEmpty() && check.IsNullOrEmpty()) return true;
 			if (source.IsNullOrEmpty() || check.IsNullOrEmpty()) return false;
@@ -181,7 +225,7 @@ namespace MyBox
 		/// <summary>
 		/// Is Values in MyDictionary is the same as some collection
 		/// </summary>
-		public static bool ValuesContentsMatch<T1, T2>(this MyDictionary<T1, T2> source, IEnumerable<T2> check)
+		public static bool ContentsMatchValues<T1, T2>(this MyDictionary<T1, T2> source, IEnumerable<T2> check)
 		{
 			if (source.IsNullOrEmpty() && check.IsNullOrEmpty()) return true;
 			if (source.IsNullOrEmpty() || check.IsNullOrEmpty()) return false;
