f778dde079d667355f96c852027b282d3e899971
Andrew <andrew@deadcow.ru>
2019-07-18 09:15:25 +0300

    I'm unable to acheive the result I want with IPrepare OnSave. For now I call Prepare right before Playmode. Will research this further

--- Tools/Features/IPrepareFeature.cs
+++ Tools/Features/IPrepareFeature.cs
@@ -1,4 +1,8 @@
-﻿namespace MyBox
+﻿using MyBox.EditorTools;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace MyBox
 {
 	public interface IPrepare
 	{
@@ -9,10 +13,13 @@
 #if UNITY_EDITOR
 namespace MyBox.Internal
 {
+	using System.Collections.Generic;
+	using System.Linq;
 	using UnityEngine;
 	using UnityEditor;
 
-	public class PrepareOnSave : AssetModificationProcessor
+	[InitializeOnLoad]
+	public class PrepareOnSave// : AssetModificationProcessor
 	{
 		private const string MenuItemName = "Tools/MyBox/Run Prepare On Save";
 		
@@ -34,26 +41,68 @@ namespace MyBox.Internal
 			Menu.SetChecked(MenuItemName, IsEnabled);
 			return true;
 		}
-		
-		private static string[] OnWillSaveAssets(string[] paths)
+
+		static PrepareOnSave()
 		{
-			if (!IsEnabled) return paths;
-			
+			MyEditorEvents.BeforePlaymode += Prepare;
+		}
+
+		static void Prepare()
+		{
 			var toPrepare = MyExtensions.FindObjectsOfInterfaceAsComponents<IPrepare>();
 
+			HashSet<Scene> modifiedScenes = null;
 			foreach (var prepare in toPrepare)
 			{
 				bool changed = prepare.Interface.Prepare();
 
 				if (changed && prepare.Component != null)
 				{
+					if (modifiedScenes == null) modifiedScenes = new HashSet<Scene>();
+					modifiedScenes.Add(prepare.Component.gameObject.scene);
+	
 					EditorUtility.SetDirty(prepare.Component);
 					Debug.Log(prepare.Component.name + "." + prepare.Component.GetType().Name + ": Changed on Prepare", prepare.Component);
 				}
 			}
 
-			return paths;
+			if (modifiedScenes != null)
+			{
+				EditorSceneManager.SaveScenes(modifiedScenes.ToArray());
+				//for (var i = 0; i < paths.Length; i++) modifiedScenes.Add(paths[i]);
+			}
 		}
+		/*
+		private static string[] OnWillSaveAssets(string[] paths)
+		{
+			if (!IsEnabled) return paths;
+
+			var toPrepare = MyExtensions.FindObjectsOfInterfaceAsComponents<IPrepare>();
+
+			HashSet<string> modifiedScenes = null;
+			foreach (var prepare in toPrepare)
+			{
+				bool changed = prepare.Interface.Prepare();
+
+				if (changed && prepare.Component != null)
+				{
+					if (modifiedScenes == null) modifiedScenes = new HashSet<string>();
+					modifiedScenes.Add(prepare.Component.gameObject.scene.path);
+	
+					EditorUtility.SetDirty(prepare.Component);
+					Debug.Log(prepare.Component.name + "." + prepare.Component.GetType().Name + ": Changed on Prepare", prepare.Component);
+				}
+			}
+
+			if (modifiedScenes != null)
+			{
+				//EditorSceneManager.SaveScenes(modifiedScenes.ToArray());
+				for (var i = 0; i < paths.Length; i++) modifiedScenes.Add(paths[i]);
+			}
+
+			return modifiedScenes == null ? paths : modifiedScenes.ToArray();
+		}
+		*/
 	}
 }
 #endif
