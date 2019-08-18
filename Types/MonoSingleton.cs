6480fee1ec1257a12113607840459704e0083504
Kaynn Cahya <kaynncahya@outlook.com>
2019-08-18 20:21:41 +0900

    Fixed Coding Style Format to suit general convention

--- Types/MonoSingleton.cs
+++ Types/MonoSingleton.cs
@@ -1,41 +1,47 @@
 using UnityEngine;
 
-namespace MyBox{
+namespace MyBox
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        [SerializeField, Tooltip("True to not destroy this object when it loads")]
+        private bool dontDestroyOnLoad;
 
-  public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour {
+        private static T _instance;
 
-    [SerializeField, Tooltip("True to not destroy this object when it loads")]
-    private bool dontDestroyOnLoad;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                    if (_instance == null)
+                    {
+                        Debug.LogError("Singleton Instance caused: " + typeof(T).Name + " not found on scene");
+                    }
+                }
 
-    private static T _instance;
-
-    public static T Instance {
-      get {
-        if (_instance == null) {
-          _instance = FindObjectOfType<T>();
-          if (_instance == null) {
-            Debug.LogError("Singleton Instance caused: " + typeof(T).Name + " not found on scene");
-          }
+                return _instance;
+            }
         }
 
-        return _instance;
-      }
-    }
+        private void Awake()
+        {
+            var instance = Instance;
 
-    private void Awake() {
-      var instance = Instance;
+            if (dontDestroyOnLoad)
+            {
+                // Ensure that the instance actually exists before we do a "DontDestroyOnLoad" on it.
+                if (instance != null)
+                {
+                    DontDestroyOnLoad(instance);
+                }
+            }
 
-      if (dontDestroyOnLoad) {
-        // Ensure that the instance actually exists before we do a "DontDestroyOnLoad" on it.
-        if (instance != null) {
-          DontDestroyOnLoad(instance);
+            OnAwake();
         }
-      }
 
-      OnAwake();
+        protected abstract void OnAwake();
     }
-
-    protected abstract void OnAwake();
-
-  }
 }
