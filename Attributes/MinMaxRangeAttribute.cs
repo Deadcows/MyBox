65f843577aab29accc0043cf2f5efe2ffe4e586b
Andrew <andrew@deadcow.ru>
2019-09-16 15:08:02 +0300

    MinMaxRange bugfix + warnings on type mismatch

--- Attributes/MinMaxRangeAttribute.cs
+++ Attributes/MinMaxRangeAttribute.cs
@@ -12,61 +12,61 @@ using UnityEditor;
 
 namespace MyBox
 {
-    public class MinMaxRangeAttribute : PropertyAttribute
-    {
-        public MinMaxRangeAttribute(float min, float max)
-        {
-            Min = min;
-            Max = max;
-        }
+	public class MinMaxRangeAttribute : PropertyAttribute
+	{
+		public MinMaxRangeAttribute(float min, float max)
+		{
+			Min = min;
+			Max = max;
+		}
 
-        public readonly float Min;
-        public readonly float Max;
-    }
+		public readonly float Min;
+		public readonly float Max;
+	}
 
-    [Serializable]
-    public struct RangedFloat
-    {
-        public float Min;
-        public float Max;
+	[Serializable]
+	public struct RangedFloat
+	{
+		public float Min;
+		public float Max;
 
-        public RangedFloat(float a, float b)
-        {
-            Min = a;
-            Max = b;
-        }
-    }
+		public RangedFloat(float min, float max)
+		{
+			Min = min;
+			Max = max;
+		}
+	}
 
-    [Serializable]
-    public struct RangedInt
-    {
-        public int Min;
-        public int Max;
+	[Serializable]
+	public struct RangedInt
+	{
+		public int Min;
+		public int Max;
 
-        public RangedInt(int a, int b)
-        {
-            Min = a;
-            Max = b;
-        }
-    }
+		public RangedInt(int min, int max)
+		{
+			Min = min;
+			Max = max;
+		}
+	}
 
-    public static class RangedExtensions 
+	public static class RangedExtensions
 	{
 		public static float LerpFromRange(this RangedFloat ranged, float t)
 		{
 			return Mathf.Lerp(ranged.Min, ranged.Max, t);
 		}
-		
+
 		public static float LerpFromRangeUnclamped(this RangedFloat ranged, float t)
 		{
 			return Mathf.LerpUnclamped(ranged.Min, ranged.Max, t);
-		}		
-		
+		}
+
 		public static float LerpFromRange(this RangedInt ranged, float t)
 		{
 			return Mathf.Lerp(ranged.Min, ranged.Max, t);
 		}
-		
+
 		public static float LerpFromRangeUnclamped(this RangedInt ranged, float t)
 		{
 			return Mathf.LerpUnclamped(ranged.Min, ranged.Max, t);
@@ -82,79 +82,74 @@ namespace MyBox.Internal
 	{
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
-            label = EditorGUI.BeginProperty(position, label, property);
-            position = EditorGUI.PrefixLabel(position, label);
-
-            SerializedProperty minProp = property.FindPropertyRelative("Min");
+			SerializedProperty minProp = property.FindPropertyRelative("Min");
 			SerializedProperty maxProp = property.FindPropertyRelative("Max");
+			if (minProp == null || maxProp == null)
+			{
+				WarningsPool.Log("MinMaxRangeAttribute used on <color=brown>" +
+				                 property.name +
+				                 "</color>. Must be used on types with Min and Max fields",
+					property.serializedObject.targetObject);
 
-            bool ifInt = false;
+				return;
+			}
 
-            float minValue;
-            float maxValue;
+			var minValid = minProp.propertyType == SerializedPropertyType.Integer || minProp.propertyType == SerializedPropertyType.Float;
+			var maxValid = maxProp.propertyType == SerializedPropertyType.Integer || maxProp.propertyType == SerializedPropertyType.Float;
+			if (!maxValid || !minValid || minProp.propertyType != maxProp.propertyType)
+			{
+				WarningsPool.Log("MinMaxRangeAttribute used on <color=brown>" +
+				                 property.name +
+				                 "</color>. Min and Max fields must be of int or float type",
+					property.serializedObject.targetObject);
 
-            if (minProp.propertyType is SerializedPropertyType.Integer) ifInt = true;
+				return;
+			}
 
-            float rangeMin = 0;
-            float rangeMax = 1;
+			MinMaxRangeAttribute rangeAttribute = (MinMaxRangeAttribute) attribute;
 
-            MinMaxRangeAttribute attr = (MinMaxRangeAttribute)base.attribute;
+			label = EditorGUI.BeginProperty(position, label, property);
+			position = EditorGUI.PrefixLabel(position, label);
 
-            rangeMin = attr.Min;
-            rangeMin = attr.Max;
+			bool isInt = minProp.propertyType == SerializedPropertyType.Integer;
 
-            if (ifInt)
-            {
-                minValue = minProp.intValue;
-                maxValue = maxProp.intValue;
+			float minValue = isInt ? minProp.intValue : minProp.floatValue;
+			float maxValue = isInt ? maxProp.intValue : maxProp.floatValue;
+			float rangeMin = rangeAttribute.Min;
+			float rangeMax = rangeAttribute.Max;
 
-                const float rangeBoundsLabelWidth = 40f;
 
-                var rangeBoundsLabel1Rect = new Rect(position);
-                rangeBoundsLabel1Rect.width = rangeBoundsLabelWidth;
-                GUI.Label(rangeBoundsLabel1Rect, new GUIContent(minValue.ToString("F0")));
-                position.xMin += rangeBoundsLabelWidth;
+			const float rangeBoundsLabelWidth = 40f;
 
-                var rangeBoundsLabel2Rect = new Rect(position);
-                rangeBoundsLabel2Rect.xMin = rangeBoundsLabel2Rect.xMax - rangeBoundsLabelWidth;
-                GUI.Label(rangeBoundsLabel2Rect, new GUIContent(maxValue.ToString("F0")));
-                position.xMax -= rangeBoundsLabelWidth;
+			var rangeBoundsLabel1Rect = new Rect(position);
+			rangeBoundsLabel1Rect.width = rangeBoundsLabelWidth;
+			GUI.Label(rangeBoundsLabel1Rect, new GUIContent(minValue.ToString(isInt ? "F0" : "F2")));
+			position.xMin += rangeBoundsLabelWidth;
 
-                EditorGUI.BeginChangeCheck();
-                EditorGUI.MinMaxSlider(position, ref minValue, ref maxValue, rangeMin, rangeMax);
-                if (EditorGUI.EndChangeCheck())
-                {
-                    minProp.intValue = Mathf.RoundToInt(minValue);
-                    maxProp.intValue = Mathf.RoundToInt(maxValue);
-                }
-            }
-            else
-            {
-                minValue = minProp.floatValue;
-                maxValue = maxProp.floatValue;
+			var rangeBoundsLabel2Rect = new Rect(position);
+			rangeBoundsLabel2Rect.xMin = rangeBoundsLabel2Rect.xMax - rangeBoundsLabelWidth;
+			GUI.Label(rangeBoundsLabel2Rect, new GUIContent(maxValue.ToString(isInt ? "F0" : "F2")));
+			position.xMax -= rangeBoundsLabelWidth;
 
-                const float rangeBoundsLabelWidth = 40f;
+			EditorGUI.BeginChangeCheck();
+			EditorGUI.MinMaxSlider(position, ref minValue, ref maxValue, rangeMin, rangeMax);
 
-                var rangeBoundsLabel1Rect = new Rect(position);
-                rangeBoundsLabel1Rect.width = rangeBoundsLabelWidth;
-                GUI.Label(rangeBoundsLabel1Rect, new GUIContent(minValue.ToString("F2")));
-                position.xMin += rangeBoundsLabelWidth;
+			if (EditorGUI.EndChangeCheck())
+			{
+				if (isInt)
+				{
+					minProp.intValue = Mathf.RoundToInt(minValue);
+					maxProp.intValue = Mathf.RoundToInt(maxValue);
+				}
+				else
+				{
+					minProp.floatValue = minValue;
+					maxProp.floatValue = maxValue;
+				}
+			}
 
-                var rangeBoundsLabel2Rect = new Rect(position);
-                rangeBoundsLabel2Rect.xMin = rangeBoundsLabel2Rect.xMax - rangeBoundsLabelWidth;
-                GUI.Label(rangeBoundsLabel2Rect, new GUIContent(maxValue.ToString("F2")));
-                position.xMax -= rangeBoundsLabelWidth;
-
-                EditorGUI.BeginChangeCheck();
-                EditorGUI.MinMaxSlider(position, ref minValue, ref maxValue, rangeMin, rangeMax);
-                if (EditorGUI.EndChangeCheck())
-                {
-                    minProp.floatValue = minValue;
-                    maxProp.floatValue = maxValue;
-                }
-            }
-            EditorGUI.EndProperty();
-        }
-    }
+			EditorGUI.EndProperty();
+		}
+	}
 }
 #endif
