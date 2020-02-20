3ed39831a46f85cfe0525bb16f8108a0af11e7b5
Andrew <andrew@deadcow.ru>
2020-02-20 13:17:20 +0300

    AnimationStateReference is now working with any specified animator, not only with the animator set on this GO

--- Types/AnimationStateReference.cs
+++ Types/AnimationStateReference.cs
@@ -2,12 +2,6 @@
 using System.Linq;
 using UnityEngine;
 
-#if UNITY_EDITOR
-using MyBox.EditorTools;
-using UnityEditor;
-using UnityEditor.Animations;
-#endif
-
 namespace MyBox
 {
 	[Serializable]
@@ -22,10 +16,16 @@ namespace MyBox
 		{
 			get { return _assigned; }
 		}
+		
+		public Animator Animator
+		{
+			get { return _linkedAnimator; }
+		}
 
 #pragma warning disable 0649
 		[SerializeField] private string _stateName = String.Empty;
 		[SerializeField] private bool _assigned;
+		[SerializeField] private Animator _linkedAnimator;
 #pragma warning restore 0649
 	}
 
@@ -36,12 +36,22 @@ namespace MyBox
 			if (!state.Assigned) return;
 			animator.Play(state.StateName);
 		}
+		
+		public static void Play(this AnimationStateReference  state)
+		{
+			if (!state.Assigned) return;
+			state.Animator.Play(state.StateName);
+		}
 	}
 }
 
 #if UNITY_EDITOR
 namespace MyBox.Internal
 {
+	using EditorTools;
+	using UnityEditor;
+	using UnityEditor.Animations;
+
 	[CustomPropertyDrawer(typeof(AnimationStateReference))]
 	public class AnimationStateReferenceDrawer : PropertyDrawer
 	{
@@ -51,23 +61,27 @@ namespace MyBox.Internal
 
 			EditorGUI.BeginProperty(position, label, property);
 
-			if (_animator == null)
-			{
-				EditorGUI.LabelField(position, label, _animatorNotFound);
-				return;
-			}
+			var widthWithoutRefresh = position.width - 34;
 
-			var popupRect = position;
-			popupRect.width -= 30;
+			var stateRect = position;
+			stateRect.width = widthWithoutRefresh / 4 * 3;
 
-			var refreshRect = position;
+			var animatorRect = position;
+			animatorRect.width = widthWithoutRefresh / 4;
+			animatorRect.x += stateRect.width + 4;
+
+			var refreshRect = animatorRect;
 			refreshRect.width = 26;
-			refreshRect.x += popupRect.width;
+			refreshRect.x += animatorRect.width + 4;
 
 
-			var state = EditorGUI.Popup(popupRect, label, CurrentIndex(), _states.Select(s => new GUIContent(s)).ToArray());
+			var state = EditorGUI.Popup(stateRect, label, CurrentIndex(), _states.Select(s => new GUIContent(s)).ToArray());
 			_stateName.stringValue = _states[state];
 			_assigned.boolValue = state > 0;
+
+			EditorGUI.BeginChangeCheck();
+			EditorGUI.ObjectField(animatorRect, _animatorProperty, GUIContent.none);
+			if (EditorGUI.EndChangeCheck()) UpdateStates();
 
 			if (GUI.Button(refreshRect, "↺")) UpdateStates();
 
@@ -76,30 +90,41 @@ namespace MyBox.Internal
 
 		private void Initialize(SerializedProperty property)
 		{
-			if (_animator == null)
-			{
-				var mb = property.GetParent() as MonoBehaviour;
-				if (mb != null)
-				{
-					_animator = mb.GetComponent<Animator>();
-					UpdateStates();
-				}
-			}
-
+			if (_animatorNotFound == null) _animatorNotFound = new GUIContent("Animator not found");
 			if (_stateName == null) _stateName = property.FindPropertyRelative("_stateName");
 			if (_assigned == null) _assigned = property.FindPropertyRelative("_assigned");
 
-			if (_animatorNotFound == null) _animatorNotFound = new GUIContent("Animator not found");
+			if (_animatorProperty == null)
+			{
+				_animatorProperty = property.FindPropertyRelative("_linkedAnimator");
+				if (_animatorProperty.objectReferenceValue == null)
+				{
+					var mb = property.GetParent() as MonoBehaviour;
+					if (mb != null)
+					{
+						var animator = mb.GetComponentInChildren<Animator>(true);
+						if (animator != null)
+						{
+							_animatorProperty.objectReferenceValue = animator;
+							_animatorProperty.serializedObject.ApplyModifiedProperties();
+						}
+					}
+				}
+
+				UpdateStates();
+			}
 		}
 
 		private void UpdateStates()
 		{
 			_states = _defaultState;
-			if (_animator == null) return;
-			var controller = _animator.runtimeAnimatorController as AnimatorController;
+			if (_animatorProperty.objectReferenceValue == null) return;
+			var animator = (Animator) _animatorProperty.objectReferenceValue;
+			var controller = animator.runtimeAnimatorController as AnimatorController;
 			if (controller != null)
 			{
-				var states = controller.layers.SelectMany(l => l.stateMachine.states).Select(s => (s.state.name)).Distinct();
+				var states = controller.layers.SelectMany(l => l.stateMachine.states)
+					.Select(s => (s.state.name)).Distinct();
 				_states = _states.Concat(states).ToArray();
 			}
 		}
@@ -113,11 +138,10 @@ namespace MyBox.Internal
 
 		private SerializedProperty _stateName;
 		private SerializedProperty _assigned;
+		private SerializedProperty _animatorProperty;
 		private GUIContent _animatorNotFound;
 
-		private Animator _animator;
 		private readonly string[] _defaultState = {"Not Assigned"};
-
 		private string[] _states = new string[1];
 	}
 }
