535e3cbb52380b9eae9d48e564937a20da3be004
Deadcow <deadcows@yandex.ru>
2020-04-06 16:28:21 +0300

    Billboard nullref fix

--- Types/Billboard.cs
+++ Types/Billboard.cs
@@ -14,7 +14,8 @@ namespace MyBox
 				if (FacedObject != null) return FacedObject;
 				if (_camera != null) return _camera.transform;
 				_camera = FindObjectOfType<Camera>();
-				return _camera.transform;
+				
+				return _camera == null ? null : _camera.transform;
 			}
 		}
 		private static Camera _camera;
