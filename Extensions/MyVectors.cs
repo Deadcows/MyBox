c95fee75b7e0650cf9b996bd403d4076163a11a8
Deadcow <deadcows@yandex.ru>
2020-07-23 12:56:30 +0300

    Vector.To summaries

--- Extensions/MyVectors.cs
+++ Extensions/MyVectors.cs
@@ -444,25 +444,51 @@ namespace MyBox
 
 		#endregion
 
+		
+		#region To
+		
+		/// <summary>
+		/// Get vector between source and destination
+		/// </summary>
 		public static Vector4 To(this Vector4 source, Vector4 destination) =>
 			destination - source;
 
+		/// <summary>
+		/// Get vector between source and destination
+		/// </summary>
 		public static Vector3 To(this Vector3 source, Vector3 destination) =>
 			destination - source;
 
+		/// <summary>
+		/// Get vector between source and destination
+		/// </summary>
 		public static Vector2 To(this Vector2 source, Vector2 destination) =>
 			destination - source;
 
+		/// <summary>
+		/// Get vector between source and target
+		/// </summary>
 		public static Vector3 To(this Component source, Component target) =>
 			source.transform.position.To(target.transform.position);
 
+		/// <summary>
+		/// Get vector between source and target
+		/// </summary>
 		public static Vector3 To(this Component source, GameObject target) =>
 			source.transform.position.To(target.transform.position);
 
+		/// <summary>
+		/// Get vector between source and target
+		/// </summary>
 		public static Vector3 To(this GameObject source, Component target) =>
 			source.transform.position.To(target.transform.position);
 
+		/// <summary>
+		/// Get vector between source and target
+		/// </summary>
 		public static Vector3 To(this GameObject source, GameObject target) =>
 			source.transform.position.To(target.transform.position);
+		
+		#endregion
 	}
 }
