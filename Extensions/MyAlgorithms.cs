e2a3a8d65842fd4588a22e46301908935f5bee13
tonygiang <tony_s_giang@yahoo.com>
2020-07-23 23:05:45 +0700

    - Convenience Cast, Is, As extension methods. - Expanded GetOrDefault for dictionaries. - MaxBy/MinBy extension methods.

--- Extensions/MyAlgorithms.cs
+++ Extensions/MyAlgorithms.cs
@@ -5,6 +5,26 @@ namespace MyBox
 	public static class MyAlgorithms
 	{
 		/// <summary>
+		/// Convert to a different type.
+		/// </summary>
+		public static R Cast<R>(this IConvertible source) =>
+			(R)Convert.ChangeType(source, typeof(R));
+
+		/// <summary>
+		/// Check if this is a particular type.
+		/// </summary>
+		public static bool Is<R>(this System.Object source) => source is R;
+
+		/// <summary>
+		/// Cast to a different type, exception-safe.
+		/// </summary>
+		public static R As<R>(this System.Object source) where R : class
+		{
+			if (source is R) return source as R;
+			return null;
+		}
+
+		/// <summary>
 		/// Take an object and pass it as an argument to a void function.
 		/// </summary>
 		public static T Pipe<T>(this T argument, Action<T> action)
