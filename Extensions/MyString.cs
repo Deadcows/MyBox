0b205e4a5865df56033cdd6074e19214bff54e55
Deadcow <deadcows@yandex.ru>
2020-07-25 12:07:08 +0300

    int.ToRoman() extension to get Roman numerial string

--- Extensions/MyString.cs
+++ Extensions/MyString.cs
@@ -5,7 +5,6 @@ namespace MyBox
 {
 	public static class MyString
 	{
-		
 		/// <summary>
 		/// "Camel case string" => "CamelCaseString" 
 		/// </summary>
@@ -13,7 +12,7 @@ namespace MyBox
 		{
 			return Regex.Replace(camelCaseString, "([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))", "$1 ").Trim();
 		}
-		
+
 		/// <summary>
 		/// "CamelCaseString" => "Camel Case String"
 		/// </summary>
@@ -30,8 +29,32 @@ namespace MyBox
 
 				return firstLetter + rest;
 			}
+
 			return firstLetter;
 		}
+		
+
+		/// <summary>
+		/// Number presented in Roman numerals
+		/// </summary>
+		public static string ToRoman(this int i)
+		{
+			if (i > 999) return "M" + ToRoman(i - 1000);
+			if (i > 899) return "CM" + ToRoman(i - 900);
+			if (i > 499) return "D" + ToRoman(i - 500);
+			if (i > 399) return "CD" + ToRoman(i - 400);
+			if (i > 99) return "C" + ToRoman(i - 100);
+			if (i > 89) return "XC" + ToRoman(i - 90);
+			if (i > 49) return "L" + ToRoman(i - 50);
+			if (i > 39) return "XL" + ToRoman(i - 40);
+			if (i > 9) return "X" + ToRoman(i - 10);
+			if (i > 8) return "IX" + ToRoman(i - 9);
+			if (i > 4) return "V" + ToRoman(i - 5);
+			if (i > 3) return "IV" + ToRoman(i - 4);
+			if (i > 0) return "I" + ToRoman(i - 1);
+			return "";
+		}
+		
 
 		/// <summary>
 		/// Surround string with "color" tag
@@ -78,7 +101,7 @@ namespace MyBox
 		/// </summary>
 		public static T AsEnum<T>(this string source, bool ignoreCase = true)
 			where T : System.Enum =>
-			(T)Enum.Parse(typeof(T), source, ignoreCase);
+			(T) Enum.Parse(typeof(T), source, ignoreCase);
 	}
 
 	public enum Colors
