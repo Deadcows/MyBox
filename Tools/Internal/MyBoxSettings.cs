b90e45e07591f465004f33f53bdb62ffabab6efe
Deadcow <deadcows@yandex.ru>
2021-04-13 17:00:16 +0300

    The way MyBoxSettings stored changed.. again. I don't know how InternalEditorUtility.LoadSerializedFileAndForget() works, but from time to time it fails, so I changed it with much simplier JSON version

--- Tools/Internal/MyBoxSettings.cs
+++ Tools/Internal/MyBoxSettings.cs
@@ -2,140 +2,115 @@
 using System;
 using System.IO;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace MyBox.Internal
 {
-	[Serializable]
-	public class MyBoxSettings : ScriptableObject
+	public static class MyBoxSettings
 	{
-		[SerializeField] private bool _autoSaveEnabled = true;
-		[SerializeField] private bool _cleanEmptyDirectoriesFeature = true;
-		[SerializeField] private bool _prepareOnPlaymode = true;
-		[SerializeField] private bool _checkForUpdates = true;
-
 		public static bool AutoSaveEnabled
 		{
-			get { return Instance._autoSaveEnabled; }
+			get => Data.AutoSaveEnabled;
 			set
 			{
-				if (Instance._autoSaveEnabled == value) return;
-				Instance._autoSaveEnabled = value;
-				Save();
+				if (Data.AutoSaveEnabled == value) return;
+				Data.AutoSaveEnabled = value;
+				SaveData(Data);
 			}
 		}
 
 		public static bool CleanEmptyDirectoriesFeature
 		{
-			get { return Instance._cleanEmptyDirectoriesFeature; }
+			get => Data.CleanEmptyDirectoriesFeature;
 			set
 			{
-				if (Instance._cleanEmptyDirectoriesFeature == value) return;
-				Instance._cleanEmptyDirectoriesFeature = value;
-				Save();
+				if (Data.CleanEmptyDirectoriesFeature == value) return;
+				Data.CleanEmptyDirectoriesFeature = value;
+				SaveData(Data);
 			}
 		}
 
 		public static bool PrepareOnPlaymode
 		{
-			get { return Instance._prepareOnPlaymode; }
+			get => Data.PrepareOnPlaymode;
 			set
 			{
-				if (Instance._prepareOnPlaymode == value) return;
-				Instance._prepareOnPlaymode = value;
-				Save();
+				if (Data.PrepareOnPlaymode == value) return;
+				Data.PrepareOnPlaymode = value;
+				SaveData(Data);
 			}
 		}
 
 		public static bool CheckForUpdates
 		{
-			get { return Instance._checkForUpdates; }
+			get => Data.CheckForUpdates;
 			set
 			{
-				if (Instance._checkForUpdates == value) return;
-				Instance._checkForUpdates = value;
-				Save();
+				if (Data.CheckForUpdates == value) return;
+				Data.CheckForUpdates = value;
+				SaveData(Data);
 			}
 		}
 
-		#region Instance
-
-		private static MyBoxSettings Instance
+		
+		[Serializable]
+		private class MyBoxSettingsData
 		{
-			get
-			{
-				if (_instance != null) return _instance;
-				_instance = LoadOrCreate();
-				return _instance;
-			}
+			// ReSharper disable MemberHidesStaticFromOuterClass
+			public bool AutoSaveEnabled = true;
+			public bool CleanEmptyDirectoriesFeature;
+			public bool PrepareOnPlaymode = true;
+			public bool CheckForUpdates = true;
+			// ReSharper restore MemberHidesStaticFromOuterClass
 		}
+
+		private static MyBoxSettingsData Data => _data ?? (_data = LoadData());
+		private static MyBoxSettingsData _data; 
+		
+		
+		#region Save Load
 
 		private static readonly string Directory = "ProjectSettings";
 		private static readonly string Path = Directory + "/MyBoxSettings.asset";
-		private static MyBoxSettings _instance;
 
-		private static void Save()
+		private static MyBoxSettingsData LoadData()
 		{
-			var instance = _instance;
+			if (!File.Exists(Path)) return new MyBoxSettingsData();
+
+			MyBoxSettingsData data;
+			try
+			{
+				var jsonData = File.ReadAllText(Path);
+				data = JsonUtility.FromJson<MyBoxSettingsData>(jsonData);
+			}
+			catch
+			{
+				data = new MyBoxSettingsData();
+				// Try parse old settings file
+				var fileContents = File.ReadAllLines(Path);
+				foreach (var content in fileContents)
+				{
+					var value = content.Split(':');
+					if (value[0].Contains("_autoSaveEnabled")) data.AutoSaveEnabled = int.Parse(value[1]) == 1;
+					if (value[0].Contains("_cleanEmptyDirectoriesFeature")) data.CleanEmptyDirectoriesFeature = int.Parse(value[1]) == 1;
+					if (value[0].Contains("_prepareOnPlaymode")) data.PrepareOnPlaymode = int.Parse(value[1]) == 1;
+					if (value[0].Contains("_checkForUpdates")) data.CheckForUpdates = int.Parse(value[1]) == 1;
+				}
+				SaveData(data);
+			} 
+			return data;
+		}
+		
+		private static void SaveData(MyBoxSettingsData data)
+		{
 			if (!System.IO.Directory.Exists(Directory)) System.IO.Directory.CreateDirectory(Directory);
 			try
 			{
-				UnityEditorInternal.InternalEditorUtility.SaveToSerializedFileAndForget(new Object[] {instance}, Path, true);
+				File.WriteAllText(Path, JsonUtility.ToJson(data, true));
 			}
 			catch (Exception ex)
 			{
 				Debug.LogError("Unable to save MyBoxSettings!\n" + ex);
 			}
-		}
-
-		private static MyBoxSettings LoadOrCreate()
-		{
-			var settings = !File.Exists(Path) ? CreateNewSettings() : LoadSettings();
-			if (settings == null)
-			{
-				DeleteFile(Path);
-				settings = CreateNewSettings();
-			}
-
-			settings.hideFlags = HideFlags.HideAndDontSave;
-
-			return settings;
-		}
-
-
-		private static MyBoxSettings LoadSettings()
-		{
-			MyBoxSettings settingsInstance;
-			try
-			{
-				settingsInstance = (MyBoxSettings) UnityEditorInternal.InternalEditorUtility.LoadSerializedFileAndForget(Path)[0];
-			}
-			catch (Exception ex)
-			{
-				Debug.LogError("Unable to read MyBoxSettings, set to defaults" + ex);
-				settingsInstance = null;
-			}
-
-			return settingsInstance;
-		}
-
-		private static MyBoxSettings CreateNewSettings()
-		{
-			_instance = CreateInstance<MyBoxSettings>();
-			Save();
-
-			return _instance;
-		}
-
-		private static void DeleteFile(string path)
-		{
-			if (!File.Exists(path)) return;
-
-			var attributes = File.GetAttributes(path);
-			if ((attributes & FileAttributes.ReadOnly) == FileAttributes.ReadOnly)
-				File.SetAttributes(path, attributes & ~FileAttributes.ReadOnly);
-
-			File.Delete(path);
 		}
 
 		#endregion
