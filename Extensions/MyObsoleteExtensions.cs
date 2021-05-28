b8aa22b7a13e10321dc36041c800b990d58fac83
Deadcow <deadcows@yandex.ru>
2021-05-28 16:00:48 +0300

    obsolete versions of renamed extensions

---
+++ Extensions/MyObsoleteExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyBox
+{
+	public static class MyObsoleteExtensions
+	{
+		[Obsolete("1.6.3: Use MyCollections.GetOrAdd instead")]
+		public static TValue GetOrDefault<TKey, TValue>(
+			this IDictionary<TKey, TValue> source,
+			TKey key,
+			TValue customDefault = default(TValue)) => source.GetOrAdd(key, customDefault);
+		
+		[Obsolete("1.6.3: Use MyCollections.GetOrAdd instead")]
+		public static TValue GetOrDefault<TKey, TValue>(
+			this IDictionary<TKey, TValue> source,
+			TKey key,
+			Func<TValue> customDefaultGenerator) => source.GetOrAdd(key, customDefaultGenerator());
+
+		[Obsolete("1.6.3: Use MyCollections.SingleToEnumerable instead")]
+		public static IEnumerable<T> AsEnumerable<T>(this T source) => source.SingleToEnumerable();
+	}
+}
