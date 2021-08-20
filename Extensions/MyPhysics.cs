2451fb1f9149715a22331d39df317bca06722bed
tonygiang <tony_s_giang@yahoo.com>
2021-08-20 19:42:43 +0700

    Add SetCameraDepthFrom, SetLossyScale, ShiftAnchor, GetAnchorCenter, GetAnchorDelta.

--- Extensions/MyPhysics.cs
+++ Extensions/MyPhysics.cs
@@ -25,5 +25,34 @@ namespace MyBox
 			source.constraints = source.constraints.BitwiseToggle(constraints, state);
 			return source;
 		}
+
+		/// <summary>
+		/// Gets a point with the same screen point on the specified Camera as the
+		/// source point, but at the specified distance from said Camera.
+		/// </summary>
+		public static Vector3 SetCameraDepthFrom(this Vector3 worldPos,
+			Camera projectingCamera,
+			float distance,
+			Camera.MonoOrStereoscopicEye eye = Camera.MonoOrStereoscopicEye.Mono)
+		{
+			var screenPoint = projectingCamera.WorldToScreenPoint(worldPos, eye);
+			return projectingCamera.ScreenToWorldPoint(screenPoint.SetZ(distance),
+				eye);
+		}
+
+		/// <summary>
+		/// Sets the lossy scale of the source Transform.
+		/// </summary>
+		public static Transform SetLossyScale(this Transform source,
+			Vector3 targetLossyScale)
+		{
+			var scaleFactorsToTarget = Vector3.Scale(targetLossyScale,
+				new Vector3(1f / source.lossyScale.x,
+					1f / source.lossyScale.y,
+					1f / source.lossyScale.z));
+			source.localScale = Vector3.Scale(source.localScale,
+				scaleFactorsToTarget);
+			return source;
+		}
 	}
 }
