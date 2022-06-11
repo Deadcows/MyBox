ff31fa39b81d3792e74bdb112aacd7cc37e6cd21
Ivan Rechkin <r1noffs@gmail.com>
2022-06-11 18:19:45 +0500

    Added conditions for physics modules into MyAlgorithms.cs

--- Extensions/MyAlgorithms.cs
+++ Extensions/MyAlgorithms.cs
@@ -131,6 +131,8 @@ namespace MyBox
 			return source;
 		}
 
+#if UNITY_PHYSICS_ENABLED
+        
 		/// <summary>
 		/// Sets the state of the source enum, chosen by the 1-bits in the specified
 		/// bit mask.
@@ -143,7 +145,11 @@ namespace MyBox
 			if (state) return source | bitMask;
 			else return source & ~bitMask;
 		}
-
+        
+#endif
+        
+#if UNITY_PHYSICS2D_ENABLED
+        
 		/// <summary>
 		/// Sets the state of the source enum, chosen by the 1-bits in the specified
 		/// bit mask.
@@ -156,5 +162,7 @@ namespace MyBox
 			if (state) return source | bitMask;
 			else return source & ~bitMask;
 		}
+        
+#endif
 	}
 }
