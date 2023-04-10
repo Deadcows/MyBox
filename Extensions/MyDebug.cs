e250b932c64c9624f807ff2dad658da80dc09b8e
Andrew Rumak <deadcows@yandex.ru>
2023-04-10 12:13:43 +0300

    MyDebug.DrawString quick fix for nullrefexception

--- Extensions/MyDebug.cs
+++ Extensions/MyDebug.cs
@@ -117,11 +117,13 @@ namespace MyBox
 		public static void DrawString(string text, Vector3 worldPos, Color? colour = null)
 		{
 #if UNITY_EDITOR
+			var view = SceneView.currentDrawingSceneView;
+			if (view == null) return;
+			
 			var defaultColor = GUI.color;
 
 			Handles.BeginGUI();
 			if (colour.HasValue) GUI.color = colour.Value;
-			var view = SceneView.currentDrawingSceneView;
 			Vector3 screenPos = view.camera.WorldToScreenPoint(worldPos);
 			Vector2 size = GUI.skin.label.CalcSize(new GUIContent(text));
 			GUI.Label(new Rect(screenPos.x - (size.x / 2), -screenPos.y + view.position.height + 4, size.x, size.y), text);
