62408bb51914a074501a094113827014b6d82b81
Andrew Rumak <deadcows@yandex.ru>
2024-01-21 14:30:41 +0300

    MyComponentUtility.MoveComponentInspectorToTop/ToBottom(Component component) utility methods

---
+++ Extensions/MyComponentUtility.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace MyBox
+{
+	[PublicAPI]
+	public static class MyComponentUtility
+	{
+		public static void MoveComponentInspectorToTop(Component component)
+		{
+#if UNITY_EDITOR
+			while (UnityEditorInternal.ComponentUtility.MoveComponentUp(component)){}
+#endif
+		}
+
+		public static void MoveComponentInspectorToBottom(Component component)
+		{
+#if UNITY_EDITOR
+			while (UnityEditorInternal.ComponentUtility.MoveComponentDown(component)){}
+#endif
+		}
+	}
+}
