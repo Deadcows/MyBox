a518d811f2c8ff0677f8da588a15a1ccf82b7b7b
Andrew Rumak <deadcows@yandex.ru>
2024-09-26 12:14:40 +0300

    MyLayer now have several handy functions and extensions to operate with LayerMasks

--- Extensions/MyLayers.cs
+++ Extensions/MyLayers.cs
@@ -1,24 +1,39 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
 
 namespace MyBox
 {
+	[PublicAPI]
 	public static class MyLayers
 	{
-		// Toggle layers lock
-		//Tools.lockedLayers = 1 << LayerMask.NameToLayer("LayerName"); // Replace the whole value of lockedLayers. 
-		//Tools.lockedLayers |= 1 << LayerMask.NameToLayer("LayerName"); // Add a value to lockedLayers. 
-		//Tools.lockedLayers &= ~(1 << LayerMask.NameToLayer("LayerName")); // Remove a value from lockedLayers. 
-		//Tools.lockedLayers ^= 1 << LayerMask.NameToLayer("LayerName")); // Toggle a value in lockedLayers.
+		public static LayerMask AddLayer(LayerMask mask, int layer) 
+			=> mask | (1 << layer);
+		public static LayerMask AddLayer(LayerMask mask, string layer) 
+			=> AddLayer(mask, LayerMask.NameToLayer(layer));
 
+		public static LayerMask RemoveLayer(LayerMask mask, int layer) 
+			=> mask & ~(1 << layer);
+		public static LayerMask RemoveLayer(LayerMask mask, string layer) 
+			=> RemoveLayer(mask, LayerMask.NameToLayer(layer));
 
-		public static LayerMask ToLayerMask(int layer)
-		{
-			return 1 << layer;
-		}
+		public static LayerMask ToggleLayer(LayerMask mask, int layer) 
+			=> mask ^ (1 << layer);
+		public static LayerMask ToggleLayer(LayerMask mask, string layer) 
+			=> ToggleLayer(mask, LayerMask.NameToLayer(layer));
 
-		public static bool LayerInMask(this LayerMask mask, int layer)
-		{
-			return ((1 << layer) & mask) != 0;
-		}
+		public static LayerMask SetLayer(LayerMask mask, int layer, bool include) 
+			=> include ? AddLayer(mask, layer) : RemoveLayer(mask, layer);
+		public static LayerMask SetLayer(LayerMask mask, string layer, bool include) 
+			=> SetLayer(mask, LayerMask.NameToLayer(layer), include);
+
+		public static LayerMask ToLayerMask(int layer) 
+			=> 1 << layer;
+		public static LayerMask ToLayerMask(string layer) 
+			=> ToLayerMask(LayerMask.NameToLayer(layer));
+
+		public static bool LayerInMask(this LayerMask mask, int layer) 
+			=> ((1 << layer) & mask) != 0;
+		public static bool LayerInMask(this LayerMask mask, string layer) 
+			=> LayerInMask(mask, LayerMask.NameToLayer(layer));
 	}
 }
