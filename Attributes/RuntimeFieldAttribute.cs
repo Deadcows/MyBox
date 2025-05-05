9efa9351f1e1a17b5889abd491f6d8a30c1e23e0
Dmitrii Sannikov <dms@pale.blue>
2025-05-05 09:42:00 +0200

    RealtimeFieldAttribute, field is not editable in edit mode

---
+++ Attributes/RuntimeFieldAttribute.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace MyBox
+{
+    /// <summary>
+    /// Field will be Read-Only in Edit mode
+    /// </summary>
+    public class RuntimeFieldAttribute : PropertyAttribute
+    {
+    }
+}
+
+#if UNITY_EDITOR
+namespace MyBox.Internal
+{
+    using UnityEditor;
+	
+    [CustomPropertyDrawer(typeof(RuntimeFieldAttribute))]
+    public class RuntimeFieldAttributeDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (!Application.isPlaying) GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
+    }
+}
+#endif
