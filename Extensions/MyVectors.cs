efb08f2318b887f8155fe654463a7669733e6611
Andrew Rumak <deadcows@yandex.ru>
2025-08-25 11:47:43 +0300

    MyVector.ToVector2XZ() and ToVector2YZ()

--- Extensions/MyVectors.cs
+++ Extensions/MyVectors.cs
@@ -168,12 +168,16 @@ namespace MyBox
 		#region Convert
 
 		public static Vector2 ToVector2(this Vector3 vector) => new Vector2(vector.x, vector.y);
+		
+		public static Vector2 ToVector2XZ(this Vector3 vector) => new Vector2(vector.x, vector.z);
+		
+		public static Vector2 ToVector2YZ(this Vector3 vector) => new Vector2(vector.y, vector.z);
 
 		public static Vector3 ToVector3(this Vector2 vector) => new Vector3(vector.x, vector.y);
 
 
 		public static Vector2 ToVector2(this Vector2Int vector) => new Vector2(vector.x, vector.y);
-
+		
 		public static Vector3 ToVector3(this Vector3Int vector) => new Vector3(vector.x, vector.y, vector.z);
 
 
