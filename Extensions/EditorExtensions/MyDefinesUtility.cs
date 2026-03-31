4a1c3f8183b1f39ab5846b6f6fc95c67b50558b8
nnra6864 <nnra2210@gmail.com>
2026-03-31 04:44:51 +0200

    2023.1 Fixes
    This is a slightly bigger commit as I extracted commonly reused code
    into more readable functions

--- Extensions/EditorExtensions/MyDefinesUtility.cs
+++ Extensions/EditorExtensions/MyDefinesUtility.cs
@@ -33,13 +33,17 @@ namespace MyBox.EditorTools
 
 			foreach (var group in buildTargetGroups)
 			{
-				var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(group).Split(';').Select(d => d.Trim())
-					.ToList();
+#if UNITY_2023_1_OR_NEWER
+				var gr = UnityEditor.Build.NamedBuildTarget.FromBuildTargetGroup(group);
+#else
+				var gr = group;
+#endif
+				var defines = GetDefines(gr);
 
 				if (!defines.Contains(define))
 				{
 					defines.Add(define);
-					PlayerSettings.SetScriptingDefineSymbolsForGroup(group, string.Join(";", defines.ToArray()));
+					SetDefines(gr, defines);
 					added = true;
 				}
 			}
@@ -53,19 +57,49 @@ namespace MyBox.EditorTools
 
 			foreach (var group in buildTargetGroups)
 			{
-				var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(group).Split(';').Select(d => d.Trim())
-					.ToList();
+#if UNITY_2023_1_OR_NEWER
+				var gr = UnityEditor.Build.NamedBuildTarget.FromBuildTargetGroup(group);
+#else
+				var gr = group;
+#endif
+				var defines = GetDefines(gr);
 
 				if (defines.Contains(define))
 				{
 					defines.Remove(define);
-					PlayerSettings.SetScriptingDefineSymbolsForGroup(group, string.Join(";", defines.ToArray()));
+					SetDefines(gr, defines);
 					removed = true;
 				}
 			}
 
 			return removed;
 		}
+		
+#if UNITY_2023_1_OR_NEWER
+		private static List<string> GetDefines(UnityEditor.Build.NamedBuildTarget namedBuildTarget)
+		{
+				PlayerSettings.GetScriptingDefineSymbols(namedBuildTarget, out var definesCol);
+				return definesCol.ToList();
+		}
+#else
+		private static List<string> GetDefines(BuildTargetGroup group)
+		{
+				return PlayerSettings.GetScriptingDefineSymbolsForGroup(group).Split(';').Select(d => d.Trim())
+					.ToList();
+		}
+#endif
+
+#if UNITY_2023_1_OR_NEWER
+		private static void SetDefines(UnityEditor.Build.NamedBuildTarget namedBuildTarget, List<string> defines)
+		{
+			PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, defines.ToArray());
+		}
+#else
+		private static void SetDefines(BuildTargetGroup group, List<string> defines)
+		{
+			PlayerSettings.SetScriptingDefineSymbolsForGroup(group, string.Join(";", defines.ToArray()));
+		}
+#endif
 	}
 }
 #endif
